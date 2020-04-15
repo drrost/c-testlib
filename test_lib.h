ac9d600f4eeeb5bc2d19e19d957add77ee7d3627
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-04-15 18:31:12 +0300

    Add t00 implementation

--- test_lib.h
+++ test_lib.h
@@ -18,9 +18,16 @@ void test_finalize();
 
 // Asserts
 
+#define ASSERT_TRUE(exp) if ((exp) == 0) { char sss[256]; \
+    sprintf(sss, "In file \"%s: %d\", function %s(): EXP: TRUE, ACR: FALSE\n", \
+    __FILE__, __LINE__, __func__); \
+    test_print_fail(sss); }
+
 #define ASSERT_EQUALS(exp, act) if (exp != act) { char sss[256]; \
     sprintf(sss, "In file \"%s: %d\", function %s(): EXP: '%d', ACR: '%d'\n", \
     __FILE__, __LINE__, __func__, exp, act); \
     test_print_fail(sss); }
+
+
 
 #endif // __TEST_LIB_H__
