10ec4029f43d094301b558109618db344ee7fa7e
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-04-17 13:47:09 +0300

    Add ASSERT_NOT_NIL

--- test_lib.h
+++ test_lib.h
@@ -18,6 +18,11 @@ void test_finalize();
 
 // Asserts
 
+#define ASSERT_NOT_NIL(exp) if ((exp) == 0) { char sss[256]; \
+    sprintf(sss, "In file \"%s: %d\", function %s(): | Passed value is nil | EXP: Not nil, ACR: 0x%x\n", \
+    __FILE__, __LINE__, __func__, (unsigned int)exp); \
+    test_print_fail(sss); }
+
 #define ASSERT_TRUE(exp) if ((exp) == 0) { char sss[256]; \
     sprintf(sss, "In file \"%s: %d\", function %s(): EXP: TRUE, ACR: FALSE\n", \
     __FILE__, __LINE__, __func__); \
