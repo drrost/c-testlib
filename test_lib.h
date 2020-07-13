e1659511aac4f531303ceacbe0436c9e9e3c255e
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-07-13 22:48:32 +0300

    Add defines for STDOUT interception

--- test_lib.h
+++ test_lib.h
@@ -39,4 +39,24 @@ void test_finalize();
     __FILE__, __LINE__, __func__, (unsigned long)exp, (unsigned long)act); \
     test_print_fail(sss); }
 
+// Intercept STDOUT
+
+#define INTERCEPT_STDOUT int stdout_bk = dup(fileno(stdout)); \
+    int pipefd[2];\
+    pipe(pipefd); \
+    dup2(pipefd[1], fileno(stdout));
+
+#define RESTORE_STDOUT fflush(stdout); \
+    close(pipefd[1]); \
+    dup2(stdout_bk, fileno(stdout)); \
+    char buf[101]; \
+    read(pipefd[0], buf, 100);
+
+#define ASSERT_STDOUT_EQUALS(s) \
+for (int i = 0; s[i]; i++) { \
+    if (s[i] != buf[i]) { \
+        ASSERT_TRUE(0); \
+    } \
+}
+
 #endif // __TEST_LIB_H__
